6eed51ee364f5ea071c0423520210bc0af2b77ba
gaelzanon <al288187@uji.es>
2025-11-27 23:06:46 +0100

    feat: implemented multiple features regarding items in a shopping list

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -19,6 +19,7 @@ const schema = a.schema({
   Item: a.model({
     name: a.string().required(),
     cost: a.float(),
+    isStriked: a.boolean(),
     listID: a.id(),
     shoppingList: a.belongsTo("ShoppingList", "listID")
   }).authorization((allow) => [
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
 import { AuthenticatorService } from '@aws-amplify/ui-angular';
-import { Router } from '@angular/router';
+import { Data, Router } from '@angular/router';
+import { HeaderService } from './header/header.service';
 
 Amplify.configure(outputs);
 
@@ -12,14 +13,32 @@ Amplify.configure(outputs);
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'amplify-angular-template';
+  header: any;
+  priceToggle: any;
 
-  constructor(public authenticator: AuthenticatorService, private router: Router) {
+  constructor(public authenticator: AuthenticatorService, public router: Router, private headerService: HeaderService) {
     Amplify.configure(outputs);
+  }
+  ngOnInit(): void {
+    if (this.priceToggle == undefined) {
+      this.header = 'My Lists';
+      this.priceToggle = false;
+    }
+    this.headerService.message$.subscribe(value => {
+      this.header = value;
+    });
   }
 
   go(url: string) {
     this.router.navigateByUrl(url);
+    this.header = 'My Lists';
+  }
+
+  toggle() {
+    this.priceToggle = !this.priceToggle;
+    this.headerService.sendToggle(this.priceToggle);
   }
 }
+
