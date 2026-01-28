faf4d6839e17aa90c1a31171cbe9717bff8ed254
gaelzanon <al288187@uji.es>
2026-01-28 19:01:40 +0100

    build: added amplify storage

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,13 +1,15 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
-import * as iam from "aws-cdk-lib/aws-iam"
+import * as iam from "aws-cdk-lib/aws-iam";
 import { data } from './data/resource';
 import { inviteUser } from './lambda-functions/invite-user/resource';
+import { storage } from './storage/resource';
 
 export const backend: any = defineBackend({
   auth,
   data,
-  inviteUser
+  inviteUser,
+  storage
 });
 
 const inviteUserLambda = backend.inviteUser.resources.lambda;
@@ -17,11 +19,33 @@ inviteUserLambda.addEnvironment(
   backend.auth.resources.userPool.userPoolId
 );
 
-const statement = new iam.PolicyStatement({
+const listUsersStatement = new iam.PolicyStatement({
   sid: "AllowListUsers",
   actions: ["cognito-idp:ListUsers"],
   resources: [backend.auth.resources.userPool.userPoolArn],
-})
+});
+
+inviteUserLambda.addToRolePolicy(listUsersStatement);
+
+// Allow authenticated users to upload & read audio files
+backend.auth.resources.authenticatedUserIamRole.addToPrincipalPolicy(
+  new iam.PolicyStatement({
+    sid: 'AllowAudioBucketAccess',
+    actions: ['s3:PutObject', 's3:GetObject'],
+    resources: [`${backend.storage.resources.bucket.bucketArn}/*`]
+  })
+);
+
+// Allow authenticated users to use AWS Transcribe
+backend.auth.resources.authenticatedUserIamRole.addToPrincipalPolicy(
+  new iam.PolicyStatement({
+    sid: 'AllowTranscribe',
+    actions: [
+      'transcribe:StartTranscriptionJob',
+      'transcribe:GetTranscriptionJob',
+    ],
+    resources: ['*'],
+  })
+);
 
 
-inviteUserLambda.addToRolePolicy(statement);
